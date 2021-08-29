d72b08a29ffbdf44f659a7d712835646a6d7df79
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-08-29 06:32:30 +0100

    added morgan for request logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,20 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan")
 const planetRouter = require("./routes/planets/planets.routes");
 
 const app = express();
 
 // middlewares
-app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan("combined"))
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "../public/index.html"));
+});
 app.use(planetRouter);
 
 module.exports = app;
