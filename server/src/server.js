4ad22ca42ed8b7a8bf999634df860ad67e74c362
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-09-02 17:04:25 +0100

    determned the nuber of cluster fork from device cpu

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,26 @@
 const http = require("http");
 const app = require("./app");
 const { loadPlanetData } = require("./models/planets.model");
+const cluster = require("cluster");
+const os = require("os");
 
 (async () => {
   const PORT = process.env.PORT || 8000;
   const server = http.createServer(app);
   await loadPlanetData();
-  server.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-  });
+
+  // console.log(os.cpus().length)
+
+  if (cluster.isMaster) {
+    console.log("spanning masters thread...");
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+      cluster.fork();
+    }
+  } else {
+    console.log("spanning worker thread...");
+    server.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+    });
+  }
 })();
