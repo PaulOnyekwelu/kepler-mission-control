a2d90301886411a6be78ba79e289466ab322ef27
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-09-06 21:44:44 +0100

    setup mongoose connection to mongodb atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,30 @@
 const http = require("http");
 const app = require("./app");
 const { loadPlanetData } = require("./models/planets.model");
-const cluster = require("cluster");
-const os = require("os");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
 
-(async () => {
+mongoose.connection.once("open", () => {
+  console.log(chalk.bgWhite.green("connected succefully to database!"))
+});
+
+
+const startServer = async () => {
   const PORT = process.env.PORT || 8000;
   const server = http.createServer(app);
+
+  // connecting to mongoose server
+  const MONGO_URI =
+    "mongodb+srv://Nasa-api-admin:EWnOpH06doalSxnZ@nasacluster.hrue0.mongodb.net/NASA-project?retryWrites=true&w=majority";
+  await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   await loadPlanetData();
 
   server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
   });
-})();
+};
+startServer();
