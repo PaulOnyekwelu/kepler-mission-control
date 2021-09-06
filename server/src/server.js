83e982364111c3bafe0af4988d6ad1fab7e4e722
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-09-06 14:02:34 +0100

    setup pm2 for cluster mode

--- server/src/server.js
+++ server/src/server.js
@@ -9,14 +9,7 @@ const os = require("os");
   const server = http.createServer(app);
   await loadPlanetData();
 
-  if (cluster.isMaster) {
-    const NUM_WORKERS = os.cpus().length;
-    for (let i = 0; i < NUM_WORKERS; i++) {
-      cluster.fork();
-    }
-  } else {
-    server.listen(PORT, () => {
-      console.log(`listening on port ${PORT}`);
-    });
-  }
+  server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
 })();
